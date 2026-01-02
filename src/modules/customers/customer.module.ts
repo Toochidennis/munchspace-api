3b66e454b4af8aeda0d24fe6d49ea56739097eae
ToochiDennis <dennistoochukwu@gmail.com>
2026-01-02 01:48:59 +0100

    swagger documentation for created endpoints added

--- src/modules/customers/customer.module.ts
+++ src/modules/customers/customer.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { CustomerService } from '@/modules/customers/customer.service';
 import { CustomerController } from '@/modules/customers/customer.controller';
 import { OtpModule } from '@/shared/infra/otp/otp.module';
+import { AuthModule } from '@/modules/auth/auth.module';
 
 @Module({
-  imports: [OtpModule],
+  imports: [OtpModule, AuthModule],
   controllers: [CustomerController],
   providers: [CustomerService],
-  exports: [CustomerService],
+  exports: [],
 })
 export class CustomerModule {}
