2cdc93c5475f98c60ce134b3bfefbd2b34d0b0a5
ToochiDennis <dennistoochukwu@gmail.com>
2026-01-03 23:05:16 +0100

    CI/CD

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -euo pipefail
 
-echo "[startup] running Prisma migrations..."
-npx prisma migrate deploy
+#echo "[startup] running Prisma migrations..."
+#npx prisma migrate deploy
 
 echo "[startup] starting api server..."
 exec node -r tsconfig-paths/register dist/src/main.js
